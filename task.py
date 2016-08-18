872b09279fb6c3826456406316b80cd92435d6b4
Armando Aguilar L <a.aguilar@gategeek.com>
2016-08-18 18:22:43 -0500

    Cree la funcion de TaskRegistroPryectos que  va a preparar la senctencia sql para la tabla AARegistroDelproyecto

--- task.py
+++ task.py
@@ -7,20 +7,7 @@ import sys
 from datetime import datetime,time
 reload(sys)
 sys.setdefaultencoding("utf-8")
-##if datos['id'] == '281337':
-#    requestTask = urllib2.Request("https://forta.teamwork.com/projects/" + str(datos['id'])  +"/tasks.json")
-#    requestTask.add_header("Authorization", "BASIC " + base64.b64encode(key + ":xxx"))
-#    responseTask = urllib2.urlopen(requestTask)
-#    datajsonTaks = json.loads(responseTask.read(),encoding='utf-8',cls=None,object_hook=None, parse_float=None,parse_int=None, parse_constant=None,object_pairs_hook=None)
-#    i = 0
 
-#    for Task in datajsonTaks['todo-items']:
-#        i += 1
-#        if Task.get('responsible-party-ids') is None:
-#            print ('[--Task('+ str(Task['id']) +'): ' + str(Task['content']) + ' Responsable: None ]')
-#        else:
-#            print ('--[Task('+ str(Task['id']) +'): ' + str(Task['content']) + ']')
-#            print ('---->Responsable: ' + str(Task['responsible-party-ids']) + ' | ' + str(Task['responsible-party-names']))
 def task(arg):
     if arg == '281337':
         requestTask = urllib2.Request("https://forta.teamwork.com/projects/" + str(arg)  +"/tasks.json")
@@ -39,7 +26,6 @@ def task(arg):
 
 def TaksTiempoDiarios(idtask):
 
-    #print('https://forta.teamwork.com/tasks/' + str(idtask) + '/time_entries.json')
     requestActivitiesTask = urllib2.Request('https://forta.teamwork.com/tasks/' + str(idtask) + '/time_entries.json')
     requestActivitiesTask.add_header("Authorization", "BASIC " + base64.b64encode(key + ":xxx"))
     responseActivitiesTask = urllib2.urlopen(requestActivitiesTask)
@@ -50,3 +36,10 @@ def TaksTiempoDiarios(idtask):
         FechaJsonArrays = str(activities['date']).split("T")
         sql = 'Insert Into AARegistroTiemposDiarios values(\'' +str(idtask) + '\',\'' + str(activities['person-id']) + '\',\'' + ProyectoArray[0] + '\',\''+ str(activities['person-first-name']) + ' ' + str(activities['person-last-name']) + '\',\'' + FechaJsonArrays[0] + '\',\'' + str(activities['hours']) + '\')'
         print (str(sql))
+
+def TaskRegistroProyectos(idproyect):
+    #https://forta.teamwork.com/projects/281337/tasks.json
+    requestProyectTask = urllib2.Request('https://forta.teamwork.com/projects/' + str(idproyect) + '/tasks.json)
+    requestProyectTask.add_header("Authorization", "BASIC " + base64.b64encode(key + ":xxx"))
+    responseProyectTask = urllib2.urlopen(requestProyectTask)
+    datajsonProyectTask = json.loads(responseProyectTask.read(),encoding='utf-8',cls=None,object_hook=None, parse_float=None,parse_int=None, parse_constant=None,object_pairs_hook=None)
