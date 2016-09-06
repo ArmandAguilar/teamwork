483abeeb018a76e76fd672717fe70a040edfa529
Armando Aguilar L <a.aguilar@gategeek.com>
2016-09-06 11:06:20 -0500

    06/09/2016 11:06 am

--- validate.py
+++ validate.py
@@ -5,11 +5,11 @@ import pymssql
 
 def validate_up_in(IdTareas,IdProyecto,IdUsuario,ListaTarea):
     sql_buscar = 'SELECT [IdTareas] FROM [SAP].[dbo].[AAARegistroProyecto] Where [IdTareas]=\'' + str(IdTareas) + '\' and [IdProyecto]=\'' + str(IdProyecto) + '\' and [IdUsuario]=\'' + str(IdUsuario) + '\' and [ListaTarea]=\'' + str(ListaTarea) + '\''
-    conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
-    cur = conn.cursor()
-    cur.execute(sql_buscar)
-    for value in cur:
-        print value
-    conn.commit()
-    conn.close()
-    return value
+    #conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
+    #cur = conn.cursor()
+    #cur.execute(sql_buscar)
+    #for value in cur:
+    #    print value
+    #conn.commit()
+    #conn.close()
+    return sql_buscar
