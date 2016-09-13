669dac8375b8fd458dfb6dbdc80fbd07d940f240
Armando Aguilar L <a.aguilar@gategeek.com>
2016-09-13 16:32:52 -0500

    13/08/2016 04:32 pm

--- sap.py
+++ sap.py
@@ -40,7 +40,8 @@ def sap_insert(DirTiempoDiario):
         #funcion que completa los metadatos del usuario
         #DirMetaDataUser = metaDataUser(TiempoDiario['IdUsuarioTeam'])
         #Funcion qu busca el nombre del proyecto registrado en la base de datos MSSQL
-        Proy = ProyectName(TiempoDiario['NumProyecto'])
+        #Proy = ProyectName(TiempoDiario['NumProyecto'])
+        Proy = str(TiempoDiario['NumProyecto'])
         #Consulta para instera en la tabla AATiemposDeProduccion
         #sql = 'INSERT INTO [SAP].[dbo].[AATiemposDeProduccion] VALUES (\'' + str(DirMetaDataUser['Nombre']) + '\',\'' + str(DirMetaDataUser['Apellidos']) + '\',\'' + str(TiempoDiario['NumProyecto']) + '\',\'NomProyecto\',\'' + str(TiempoDiario['Dia']) + '\',\'' + str(TiempoDiario['Tarea']) + '\',\'Porcentaje\',\'Producto\',\'' + str(dataDir['IdUsuario']) + '\',\'0\',\'' + str(DirMetaDataUser['Departamento']) + '\',\'' + str(DirMetaDataUser['Perfil']) + '\',\'.\',\'' + str(DirMetaDataUser['Acronimo']) + '\',\'Si\')'
 
