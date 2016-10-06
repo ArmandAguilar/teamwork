a001f3e3d15b0c8027eb088e24354865789b3a8c
Armando Aguilar L <a.aguilar@gategeek.com>
2016-10-06 16:18:17 -0500

    06/10/016 - 04:18

--- task.py
+++ task.py
@@ -181,7 +181,7 @@ def ReordenarSAP():
             #print(str(Fecha) + '-' + str(Es100))
             #if Es100 == 'No':
             DirSAP['NumProyecto'] = IdProyecto
-            DirSAP['Dia'] = str(Fecha).remplace('/','-')
+            DirSAP['Dia'] = str(Fecha).replace('/','-')
             DirSAP['Tarea'] = Descripcion
             DirSAP['IdUsuarioTeam'] = IdUsuario
             DirSAP['Horas'] = Tiempo
