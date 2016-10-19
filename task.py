a1fcdb1991517d8eecc00903b669a8622e58e044
Armando Aguilar L <a.aguilar@gategeek.com>
2016-10-19 13:17:12 -0500

    19/10/2016 1:17pm

--- task.py
+++ task.py
@@ -130,12 +130,13 @@ def TaskRegistroProyectos(idproyect):
                 DueDate = datetime.strptime(str(ProyectTask['due-date']),'%Y%m%d')
         #Aqui se programa un diccionario para validar los dias y insterar o actualizar el regisro
         tipoConsulta = ''
-
+        Tarea = ProyectTask['content']
+        Tarea = str(Tarea).replace('\'','.')
+        print (str(Tarea))
         #IdUsuario
         try:
             int(IdResposnable)
-            Tarea = ProyectTask['content']
-            Tarea = str(Tarea).replace('"','.')
+
             tipoConsulta = validate_up_in(str(ProyectTask['id']),ProyectoArray[0],str(IdResposnable),str(ParentTask))
             if tipoConsulta == 'Insert':
                 sql = 'Insert into AAARegistroProyecto values(\''  + str(ProyectTask['id']) + '\',\'' + ProyectoArray[0] +'\',\'' + str(IdResposnable) + '\',\''+ str(Tarea) + '\',\'' + str(ParentTask) + '\',\'' + str(StartDate) + '\',\'' + str(DueDateBase) + '\',\'' + str(DueDate) + '\',\'' + str(ProyectTask['progress']) + '\',\'' + str(ProyectTask['completed']) + '\',\'EtiqFase\',\'EtiqDocumento\',\'EtiqDisciplina\',\'' + str(ProyectTask['description']) + '\',\'' + str(ProyectTask['estimated-minutes']) + '\')'
@@ -154,8 +155,8 @@ def TaskRegistroProyectos(idproyect):
                 else:
                     #update
                     sql = 'UPDATE [SAP].[dbo].[AAARegistroProyecto] SET [Tarea] = \'' + str(Tarea) + '\',[FechaIncio] = \'' + str(StartDate) + '\',[FechaFinalProgramada] = \'' + str(DueDateBase) + '\',[FehaFinalR] = \'' + str(DueDate) + '\',[Avance] = \'' + str(ProyectTask['progress']) + '\',[Completada] = \'' + str(ProyectTask['completed']) + '\',[EtqFase] = \'----\',[EtqDocumento] = \'---\',[EtqDiciplina] = \'---\',[Cantidad] = \'\',[TiempoEstimado] = \'\' WHERE [IdTareas]=\'' + str(ProyectTask['id']) + '\' and [IdProyecto]=\'' + ProyectoArray[0] + '\' and [IdUsuario]=\'' + str(idUser) + '\' and [ListaTarea]=\'' + str(ParentTask) + '\''
-                ##sql_sentencia(sql)
-                print (str(sql))
+                #sql_sentencia(sql)
+                #print (str(sql))
 #Esta funcione lee los cambio de la tabla AAARegistroDeTiemposDiarios para insertar en la tabla de sap
 
 def ReordenarSAP():
