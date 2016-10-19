09cd9cb179e9f443c34f042e09e40e2f09aec73d
Armando Aguilar L <a.aguilar@gategeek.com>
2016-10-19 13:43:05 -0500

    19/10/2016 1:43pm

--- task.py
+++ task.py
@@ -130,9 +130,15 @@ def TaskRegistroProyectos(idproyect):
                 DueDate = datetime.strptime(str(ProyectTask['due-date']),'%Y%m%d')
         #Aqui se programa un diccionario para validar los dias y insterar o actualizar el regisro
         tipoConsulta = ''
-        Tarea = str(ProyectTask['description'])
-        Tarea = str(Tarea).replace('\'','.')
-        print (str(Tarea))
+        #Eliminos los caratres peligroso para la consulta
+        Tarea = ProyectTask['content']
+        Tarea = str(Tarea).replace('\'',' ')
+        Tarea = str(Tarea).replace('"',' ')
+
+        Descripcion = str(ProyectTask['description'])
+        Descripcion = str(Descripcion).replace('\'',' ')
+        Descripcion = str(Tarea).replace('"'',' ')
+        print (str(Descripcion))
         #IdUsuario
         try:
             int(IdResposnable)
@@ -142,7 +148,7 @@ def TaskRegistroProyectos(idproyect):
                 sql = 'Insert into AAARegistroProyecto values(\''  + str(ProyectTask['id']) + '\',\'' + ProyectoArray[0] +'\',\'' + str(IdResposnable) + '\',\''+ str(Tarea) + '\',\'' + str(ParentTask) + '\',\'' + str(StartDate) + '\',\'' + str(DueDateBase) + '\',\'' + str(DueDate) + '\',\'' + str(ProyectTask['progress']) + '\',\'' + str(ProyectTask['completed']) + '\',\'EtiqFase\',\'EtiqDocumento\',\'EtiqDisciplina\',\'' + str(ProyectTask['description']) + '\',\'' + str(ProyectTask['estimated-minutes']) + '\')'
             else:
                 sql = 'UPDATE [SAP].[dbo].[AAARegistroProyecto] SET [Tarea] = \'' + str(Tarea) + '\',[FechaIncio] = \'' + str(StartDate) + '\',[FechaFinalProgramada] = \'' + str(DueDateBase) + '\',[FehaFinalR] = \'' + str(DueDate) + '\',[Avance] = \'' + str(ProyectTask['progress']) + '\',[Completada] = \'' + str(ProyectTask['completed']) + '\',[EtqFase] = \'----\',[EtqDocumento] = \'---\',[EtqDiciplina] = \'---\',[Cantidad] = \'\',[TiempoEstimado] = \'\' WHERE [IdTareas]=\'' + str(ProyectTask['id']) + '\' and [IdProyecto]=\'' + ProyectoArray[0] + '\' and [IdUsuario]=\'' + str(IdResposnable) + '\' and [ListaTarea]=\'' + str(ParentTask) + '\''
-            #print (str(sql))
+            print (str(sql))
             ##sql_sentencia(sql)
         except ValueError:
             #print ('cadean' + '.-' + str(IdResposnable))
