cd492548924b620fe632452ce03d74bb2fd37930
Armando Aguilar L <a.aguilar@gategeek.com>
2016-10-27 17:30:19 -0500

    27/10/2016 5:30pm

--- mensajes.py
+++ mensajes.py
@@ -6,10 +6,10 @@ from pyfiglet import figlet_format
 
 
 def cargardo():
-    cprint(figlet_format('Procesando', font='starwars'),'yellow', 'on_green', attrs=['normal'])
+    cprint(figlet_format('Procesando', font='slant'),'yellow', 'on_green', attrs=['bold'])
 
 def proceso(texto):
-    cprint(figlet_format(texto, font='starwars'),'yellow', 'on_red', attrs=['normal'])
+    cprint(figlet_format(texto, font='slant'),'yellow', 'on_red', attrs=['bold'])
 
 def proceso_terminado():
-    cprint(figlet_format('Procesando', font='starwars'),'yellow', 'on_blue', attrs=['normal'])
+    cprint(figlet_format('Termino', font='slant'),'yellow', 'on_blue', attrs=['bold'])
