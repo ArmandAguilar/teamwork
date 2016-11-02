384d7a284d441b72368ad93b843eebae064f6c76
Armando Aguilar L <a.aguilar@gategeek.com>
2016-11-02 16:25:24 -0600

    02/11/2016 04:25 pm

--- task.py
+++ task.py
@@ -122,8 +122,8 @@ def TaskRegistroProyectos(idproyect):
         #IdUsuario
         try:
             int(IdResposnable)
-
             tipoConsulta = validate_up_in(str(ProyectTask['id']),str(ProyectoArray[0]),str(IdResposnable),str(ParentTask))
+            print(tipoConsulta)
             if tipoConsulta == 'Insert':
                 sql = 'Insert into AAARegistroProyecto values(\''  + str(ProyectTask['id']) + '\',\'' + ProyectoArray[0] +'\',\'' + str(IdResposnable) + '\',\''+ str(Tarea) + '\',\'' + str(ParentTask) + '\',\'' + str(StartDate) + '\',\'' + str(DueDateBase) + '\',\'' + str(DueDate) + '\',\'' + str(ProyectTask['progress']) + '\',\'' + str(ProyectTask['completed']) + '\',\'EtiqFase\',\'EtiqDocumento\',\'EtiqDisciplina\',\'' + str(Descripcion) + '\',\'' + str(ProyectTask['estimated-minutes']) + '\')'
             else:
--- validate.py
+++ validate.py
@@ -8,13 +8,13 @@ def validate_up_in(IdTareas,IdProyecto,IdUsuario,ListaTarea):
     accion = 'Insert'
     sql_buscar = 'SELECT [IdTareas] FROM [SAP].[dbo].[AAARegistroProyecto] Where [IdTareas]=\'' + str(IdTareas) + '\' and [IdProyecto]=\'' + str(IdProyecto) + '\' and [IdUsuario]=\'' + str(IdUsuario) + '\' and [ListaTarea]=\'' + str(ListaTarea) + '\''
     #print(sql_buscar)
-    conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
-    cur = conn.cursor()
-    cur.execute(sql_buscar)
-    for value in cur:
-        accion = 'Update'
-    conn.commit()
-    conn.close()
+    #conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
+    #cur = conn.cursor()
+    #cur.execute(sql_buscar)
+    #for value in cur:
+    #    accion = 'Update'
+    #conn.commit()
+    #conn.close()
     return sql_buscar
 
 def validate_up_in_AAARegistroDeTiemposDiarios(idTaskTeamwork):
