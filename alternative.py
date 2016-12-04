1944154c5ea42081a955cf0f0fa566d841b623d1
Armando Aguilar L <a.aguilar@gategeek.com>
2016-12-04 01:44:19 -0600

    Script aletrative.py

---
+++ alternative.py
@@ -0,0 +1,62 @@
+#!user/bin/python
+from tokens import *
+from projects import *
+from sap import *
+from mssql import *
+import pypyodbc as pyodbc
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+#def validar_si_exiete(idUsuario,Dia,Procentaje):
+#    Accion = 'No'
+#    Fecha = str(Dia).replace('/','-')
+#    sql_buscar = 'SELECT [Porcentaje] FROM [SAP].[dbo].[AATiemposDeProduccionClon] Where [IdUsuario] = \'' + str(idUsuario) + '\' and Dia = \'' + str(Fecha) + '\' and Porcentaje = \'' + str(Procentaje) + '\''
+#    con = pyodbc.connect(constr)
+#    cur = con.cursor()
+#    cur.execute(sql_buscar)
+#    for value in cur:
+#         Accion = 'Si'
+#    con.commit()
+#    con.close()
+
+#    return Accion
+
+
+def Tiempos_TemaWork(IdProyecto):
+    requestTiempo = urllib2.Request('https://forta.teamwork.com/projects/' + IdProyecto + '/time.json')
+    requestTiempo.add_header("Authorization", "BASIC " + base64.b64encode(key + ":xxx"))
+    responseTiempo = urllib2.urlopen(requestTiempo)
+    datajsonTiempo = json.loads(responseTiempo.read(),encoding='utf-8',cls=None,object_hook=None, parse_float=None,parse_int=None, parse_constant=None,object_pairs_hook=None)
+    PorcentajeF = 0.0
+    for dataValor in datajsonTiempo['time-entries']:
+
+        ProyectoArray = str(dataValor['project-name']).split(" ")
+        Fecha = str(dataValor['date']).split("T")
+        DescripcionUser = str(dataValor['description'])
+        DescripcionUser = str(DescripcionUser).replace('\'',' ')
+        DescripcionUser = str(DescripcionUser).replace('"',' ')
+        DescripcionUser = str(DescripcionUser).strip()
+
+        #Metadatos de SAP
+        DirMetaDataUser = metaDataUser(str(dataValor['person-id']))
+
+        #Calculamos el Porcenta y el Costo
+        Horas = float(dataValor['hours'])
+        Minutos = float(dataValor['minutes']) / 60
+        HorasReal = Horas + Minutos
+        if HorasReal > 9:
+            Porcentaje = (HorasReal/HorasReal) * 100
+        else:
+            Porcentaje = (HorasReal/9.0) * 100
+
+        PorcentajeF = float("{0:.2f}".format(Porcentaje))
+        #Producto = (PorcentajeF * float(Costo)) / 100
+        #ProductoF = float("{0:.2f}".format(Producto))
+
+        sqlInsert = 'INSERT INTO [SAP].[dbo].[AATiemposDeProduccionClon] VALUES (\'' + str(DirMetaDataUser['Nombre']) + '\',\'' + str(DirMetaDataUser['Apellidos']) + '\',\'' + str(ProyectoArray[0]) + '\',\'NomProyecto\',\'' + str(Fecha) + '\',\'' + str(DescripcionUser) + '\' ,\'' + str(PorcentajeF) + '\' ,\'Producto\',\'' + str(DirMetaDataUser['IdUsuario']) + '\',\'0\',\'' + str(DirMetaDataUser['Departamento']) + '\',\'' + str(DirMetaDataUser['Perfil']) + '\',\'Titulo\',\'' + str(DirMetaDataUser['Perfil'])+ '\',\'Si\',\'' + str(dataValor['id']) + '\')'
+        sqlUpdate = 'UPDATE [SAP].[dbo].[AATiemposDeProduccionClon] SET [Nombre] = \'' +  str(DirMetaDataUser['Nombre']) + '\',[Apellidos] = \'' + str(DirMetaDataUser['Apellidos']) + '\' [NumProyecto] = \'' + str(ProyectoArray[0]) + '\',[NomProyecto] = \'\',[Dia] = \'' + str(Fecha) + '\',[Tarea] = \'' + str(DescripcionUser) + '\',[Porcentaje] = \'' + str(PorcentajeF) + '\',[Producto] = \'\',[IdUsuario] = \'' + str(DirMetaDataUser['IdUsuario']) + '\',[Departamento] = \'' + str(DirMetaDataUser['Departamento'])  + '\',[Perfil] = \'' + str(DirMetaDataUser['Perfil']) + '\',[Titulo] = \'.\',[Acronimo] = \'' + str(DirMetaDataUser['Acronimo']) + '\' WHERE IdTemaWork=\'' + str(dataValor['id']) + '\''
+        print(sqlInsert)
+
+for proyecto in projectos_id:
+
+    Tiempos_TemaWork(proyecto)
