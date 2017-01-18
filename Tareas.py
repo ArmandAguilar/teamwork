5ab231c15178d95991b729d6fdfbd498f33f433c
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-18 12:03:03 -0600

    Fix in instert AATareasTeamWork

--- Tareas.py
+++ Tareas.py
@@ -64,7 +64,7 @@ def allTaskCompleted(IdProyecto):
                         procesar_mssql(Sql)
                         print(Sql)
                     else:
-                        Sql = 'INSERT INTO [SAP].[dbo].[AATareasTeamWork] VALUES(\'' + str(ProyectoArray[0]) + '\',\'' + str(IdUsuario) + '\',\'' + str(TaskWords) + '\',\'' + str(dataValor['id']) + '\')'
+                        Sql = 'INSERT INTO [SAP].[dbo].[AATareasTeamWork] VALUES(\'' + str(ProyectoArray[0]) + '\',\'' + str(IdUsuario) + '\',\'' + str(dataValor['id']) + '\',\'' + str(TaskWords) + '\')'
                         procesar_mssql(Sql)
                         print(Sql)
         Paginado += 1
