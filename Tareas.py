46a546d24b09d1a7e702251d967dfe9b2344505f
Armando Aguilar L <a.aguilar@gategeek.com>
2017-01-20 13:49:43 -0600

    Script Tareas.py 01:49 pm

--- Tareas.py
+++ Tareas.py
@@ -64,7 +64,7 @@ def allTaskCompleted(IdProyecto):
                     TaskWords = str(TaskWord[:300])
                     #FechaInicio = datetime.strptime(str(dataValor['start-date']),'%Y%m%d')
                     AFI = str(dataValor['start-date'])
-                    AFI = str(AFI[:3])
+
                     FechaProgramada = str(dataValor['due-date'])
                     FechaFinal = str(dataValor['created-on'])
                     Avance = str(dataValor['progress'])
@@ -75,7 +75,7 @@ def allTaskCompleted(IdProyecto):
                         print(Sql)
                     else:
                         va = 0
-                        Sql = 'INSERT INTO [SAP].[dbo].[AATareasTeamWork] VALUES (\'' + str(ProyectoArray[0]) + '\',\'' + str(IdUsuario) + '\',\'' + dataValor['id'] + '\',\'' + str(TaskWords) + '\',\'' + AFI + '\',\'FechaProgramada\',\'FechaFinal\',\'Avance\',\'EtiqFase\',\'EtiqDocumento\',\'EtiqDiciplica\',\'TiempoEstimado\',\'Evaluada\')'
+                        Sql = 'INSERT INTO [SAP].[dbo].[AATareasTeamWork] VALUES (\'' + str(ProyectoArray[0]) + '\',\'' + str(IdUsuario) + '\',\'' + dataValor['id'] + '\',\'' + str(TaskWords) + '\',\'' + str(AFI) + '\',\'FechaProgramada\',\'FechaFinal\',\'Avance\',\'EtiqFase\',\'EtiqDocumento\',\'EtiqDiciplica\',\'TiempoEstimado\',\'Evaluada\')'
                         #procesar_mssql(Sql)
                         print(Sql)
         Paginado += 1
