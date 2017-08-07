17f126072b03ddde211a55fb0976e3d5f20bf55a
Armando Aguilar L <a.aguilar@gategeek.com>
2017-08-07 15:26:56 -0500

    I delete script that we dont need

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-#!user/bin/python
-import sys
 from tokens import *
 from projects import *
-from task import *
+from sap import *
 from mssql import *
+import pypyodbc as pyodbc
 import urllib2, base64
 import json
 import unicodedata
-import pypyodbc as pyodbc
-import pymssql
-from colorama import init
-from termcolor import cprint
-from pyfiglet import figlet_format
-from mensajes import *
+import sys
+from datetime import datetime,time
+reload(sys)
 sys.setdefaultencoding("utf-8")
+
+
 print ("############################ Start Sync Datos Teamwork ############################")
 
-cargardo()
+import ThroughputClientes
 
-proceso('Corrigiendo Tiempo Diarios')
 
 print ("############################ End Sync Datos Teamwork  ############################")
