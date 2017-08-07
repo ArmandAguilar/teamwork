93e01ada4a116fb5c854c9483befdd5e9ad717fa
Armando Aguilar L <a.aguilar@gategeek.com>
2017-08-07 13:10:53 -0500

    i create script main.py

--- main.py
+++ main.py
@@ -15,23 +15,9 @@ from pyfiglet import figlet_format
 from mensajes import *
 sys.setdefaultencoding("utf-8")
 print ("############################ Start Sync Datos Teamwork ############################")
-#Borramos datos de las tablas :
-#sql_delete_RegistoTiemposDiarios()
-#sql_delete_RegistroProyectos()
-#1.- Lista de Proyectos
+
 cargardo()
 
-for proyecto in projectos_id:
-#2.- Buscamos las tareas de cada proyectos
-    task(proyecto)
-    TaskRegistroProyectos(proyecto)
-    #print (proyecto)
-
 proceso('Corrigiendo Tiempo Diarios')
-EliminarCambioEnTiemposDiarios()
 
-#proceso('Procsnado SAP')
-#ParaSAP()
-
-proceso_terminado()
 print ("############################ End Sync Datos Teamwork  ############################")
